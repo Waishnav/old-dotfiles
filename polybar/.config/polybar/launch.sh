506bc74a7268adab9d6d8670fcc3f5925c1f2d8f
Waishnav <waishnavdeore@gmail.com>
2021-11-14 11:54:41 +0530

    updating configs

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,12 +1,15 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 # Terminate already running bar instances
 killall -q polybar
-# If all your bars have ipc enabled, you can also use 
-# polybar-msg cmd quit
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-#polybar black >>/tmp/polybar1.log 2>&1 & disown
-polybar example -r >>/tmp/polybar1.log 2>&1 & disown
-echo "Bars launched..."
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar
+polybar example &
+
+my_laptop_external_monitor=$(xrandr --query | grep 'HDMI-1')
+if [[ $my_laptop_external_monitor = *connected* ]]; then
+    polybar example_extrnl &
+fi
